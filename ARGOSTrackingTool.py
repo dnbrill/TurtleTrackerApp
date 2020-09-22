a54398ed37c084da77a1307aae367c32e18ff016
dnbrill <dnb22@duke.edu>
2020-09-22 19:27:57 -0400

    Initial commit of AFGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Debbie Brill (deborah.brill@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
+
+
