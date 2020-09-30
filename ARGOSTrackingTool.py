99dfc32d68ba67b72c9c412c71885c2833402ae4
dnbrill <dnb22@duke.edu>
2020-09-30 11:09:15 -0400

    filters out not in 1,2,3

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -20,6 +20,10 @@ line_list = file_object.readlines()
 #close the file
 file_object.close()
 
+# Create two empty dictionary objects
+date_dict = {}
+coord_dict = {}
+
 #iterate through all lines in the lineList
 for lineString in line_list:
     if lineString[0] == "#" or lineString[0] == "u": continue
@@ -30,8 +34,12 @@ for lineString in line_list:
     record_id = lineData[0]
     obs_date = lineData[2]
     obs_lc = lineData[4]
+    if obs_lc not in ("1","2","3"): continue
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
+    date_dict[record_id] = obs_date
+    coord_dict[record_id] = (obs_lat, obs_lon)
+    
